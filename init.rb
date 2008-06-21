31a11556e52fa41f4b96b72278cca7ecbb01dc8b
Scott Patten <scott@ruboss.com>
2008-06-20 21:12:03 -0700

    uncommenting ActionController::Base.allow_forgery_protection = false again in init.rb

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'ruboss_rails_integration'
 ActionView::Base.send :include, RubossHelper
 
 # temporarily disable forgery protection site-wise
-# ActionController::Base.allow_forgery_protection = false
+ActionController::Base.allow_forgery_protection = false
 
 ActionController::Base.send :include, RubossController
 ActionController::Base.send :prepend_before_filter, :extract_metadata_from_params  
